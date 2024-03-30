ea6ac17e57522bff7330e57d7858da2964089d51
Emzzy241 <emzzyoluwole@gmail.com>
2024-03-30 05:12:01 +0100

    made new changes and added UI

--- FindAndReplace/Models/WordReplacer.cs
+++ FindAndReplace/Models/WordReplacer.cs
@@ -60,22 +60,35 @@ namespace FindAndReplace.Models
     public string FindAndReplaceWord(string sentence, string oldWord, string newWord)
     {
         // Split the sentence into words
-        string[] words = sentence.Split(' ');
+        string[] arrayOfWords = sentence.Split(' ');
 
-        // Loop through each word to find and replace partial matches
-        for (int i = 0; i < words.Length; i++)
+        // Loop through each word in the array to find and replace partial matches or normal matches
+        for (int i = 0; i < arrayOfWords.Length; i++)
         {
             // If the current word contains the old word, replace it with the new word
-            if (words[i].IndexOf(oldWord, StringComparison.OrdinalIgnoreCase) != -1)
+            if (arrayOfWords[i].IndexOf(oldWord, StringComparison.OrdinalIgnoreCase) != -1)
             {
                 // Replace the old word with the new word
-                words[i] = words[i].Replace(oldWord, newWord, StringComparison.OrdinalIgnoreCase);
+                arrayOfWords[i] = arrayOfWords[i].Replace(oldWord, newWord, StringComparison.OrdinalIgnoreCase);
             }
         }
 
-        // Join the words back into a sentence
-        string modifiedSentence = string.Join(" ", words);
+        // Join the arrayOfWords back into a sentence
+        string modifiedSentence = string.Join(" ", arrayOfWords);
         return modifiedSentence;
+
+        // In the ode above:
+        /* if statement that checks whether the current word (words[i]) contains the old word (oldWord). This check is performed using the IndexOf method.
+        IndexOf returns the zero-based index of the first occurrence of the specified string within the current word. If the word doesn't contain the old word, it returns -1.
+        The condition words[i].IndexOf(oldWord, StringComparison.OrdinalIgnoreCase) != -1 checks if the index of the old word in the current word is not -1, indicating that the old word is found within the current word.
+        Replace Method:
+
+        If the old word is found within the current word, the Replace method is called to replace all occurrences of the old word with the new word.
+        The Replace method takes three parameters: the string to replace (oldWord), the replacement string (newWord), and an optional parameter (StringComparison.OrdinalIgnoreCase) specifying the string comparison rules (in this case, a case-insensitive comparison).
+        Joining Words:
+
+        After all modifications are done, the modified array of words (words) is joined back into a single string using the string.Join method. Each word is separated by a space.
+        */
     }
 
 }
--- FindAndReplace/Program.cs
+++ FindAndReplace/Program.cs
@@ -1,11 +1,70 @@
 using System;
+using FindAndReplace.Models;
 
-namespace FindAndReplace
+namespace FindAndReplace.Models
 {
     public class Program
     {
         public static void Main()
         {
+            Console.WriteLine("Welcome to the FindAndReplace App");
+            Console.WriteLine();
+            Console.WriteLine("made by Dynasty");
+
+            // Creating new Instance of class
+            Console.WriteLine("Enter 3 words; first is a sentence, second is a word you want to in that sentence, 3rd is th enew word you want to change it into");
+            Console.WriteLine();
+            Console.WriteLine("If word does not exist in sentence, the sentence gets returned and nothing will be done");
+            Console.WriteLine();
+
+            Console.WriteLine("Enter the Sentence");
+            string userSentence = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.WriteLine("Enter the Word you want to find in the sentence");
+            string userFindWord = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.WriteLine("Enter the replace word i.e the new word you want to replace that previous word into");
+            string userReplaceWord = Console.ReadLine();
+            WordReplacer newReplacer = new WordReplacer(userSentence, userFindWord, userReplaceWord);
+
+            string modifiedSentence = newReplacer.FindAndReplaceWord(userSentence, userFindWord, userReplaceWord);
+            Console.WriteLine();
+            Console.WriteLine("The modified sentence is: ");
+            Console.WriteLine(modifiedSentence);
+            Console.WriteLine();
+            Console.WriteLine();
+            try
+            {
+                Console.WriteLine("To go again, Enter 1,  to quit or exit enter 0 ");
+                string userContinue = Console.ReadLine();
+                int userContinueInt = int.Parse(userContinue);
+                 switch (userContinueInt)
+                {
+                    case 1:
+                        Main();
+                        break;
+                    case 2:
+                        Console.WriteLine("Goodbye :(");
+                        break;
+                    default:
+                        Console.WriteLine("Oops! I didn't get that, let's try again");
+                        Main();
+                        break;
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Oops! Please ensure you enter an integer(e.g: 1,2,3,4) next time");
+                Console.WriteLine();
+                Console.WriteLine("Let's give it another try");
+                Main();
+            }
+
+           
+            
+
             
         }        
     }
